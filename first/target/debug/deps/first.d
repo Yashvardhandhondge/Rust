335f5a2c5efff0e227c42bf6f93f58629c531c42
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-10-10 22:53:26 +0530

    first commit

---
+++ first/target/debug/deps/first.d
@@ -0,0 +1,5 @@
+C:\Users\yashv\OneDrive\desktop\Rust\harkirat_video\first\target\debug\deps\first.exe: src/main.rs
+
+C:\Users\yashv\OneDrive\desktop\Rust\harkirat_video\first\target\debug\deps\first.d: src/main.rs
+
+src/main.rs:
---
+++ first/target/debug/first.d
@@ -0,0 +1,1 @@
+C:\Users\yashv\OneDrive\desktop\Rust\harkirat_video\first\target\debug\first.exe: C:\Users\yashv\OneDrive\desktop\Rust\harkirat_video\first\src\main.rs
